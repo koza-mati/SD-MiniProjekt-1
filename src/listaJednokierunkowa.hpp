29140b65716eeff8290bfa1635d1013eacacfb90
Mateusz <mateuszkozik2804@gmail.com>
2026-03-12 23:18:15 +0100

    feat(lita_jednokier): Added hpp file to listaJednokierunkowa with varaiables and functions without implementation

---
+++ src/listaJednokierunkowa.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct Node{
+    int element; // zmienna przechowująca wartość elementu
+    Node* next; // wskaźnik na następny element listy
+};
+
+class listaJednokierunkowa{
+    private:
+        Node* head; // wskaźnik na pierwszy element listy
+        Node* tail; // wskaźnik na ostatni element listy
+    public:
+        listaJednokierunkowa(); // konstruktor
+        ~listaJednokierunkowa(); // destruktor
+        bool isEmpty() const; // sprawdza czy lista jest pusta
+        void addToFront(const int& element); // dodaje element na początek listy
+        void addAtEnd(const int& element); // dodaje element na koniec listy
+        void removeFromBegining(); // usuwa element z początku listy
+        void removeFromEnd(); // usuwa element z końca listy
+        void addAtPosition(const int& element, int position); // dodaje element na określoną pozycję
+        void removeFromPosition(int position); // usuwa element z określonej pozycji
+        bool isInList(const int& element) const; // sprawdza czy element znajduje się w liście
+};
